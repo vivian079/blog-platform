5865e0cb97ad3c8b55f00d8d152490869827a66a
yujie79 <yujiezhang079@gmail.com>
2024-03-13 11:25:14 -0500

    add a middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,19 @@
-import User from '../model/user.model.js';
-import bcryptjs from 'bcryptjs';
+import User from "../model/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  if(!username || !email || !password || username === '' || email === '' || password === ''){
-    return res.status(400).json({message: 'All fields are required'});
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -18,10 +26,8 @@ export const signup = async (req, res) => {
 
   try {
     await newUser.save();
-    res.json('Signup successful');
-  } catch(error){
-    res.status(500).json({message: error.message});
+    res.json("Signup successful");
+  } catch (error) {
+    next(error);
   }
-
-  
-}
+};
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -23,3 +23,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
